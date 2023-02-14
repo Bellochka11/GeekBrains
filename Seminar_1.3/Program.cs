e7b73cfa05e6581698e8fabd1f9bc688dc84f5e6
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-14 19:25:20 +0300

    Семинар 1. На вход число N а на выходе целые числа от -N до N

---
+++ Seminar_1.3/Program.cs
@@ -0,0 +1,10 @@
+﻿// На вход число N а на выходе целые числа от -N до N
+Console.WriteLine("Введите число: ");
+int  n = int.Parse(Console.ReadLine()!);
+ int count = -n;
+while (count <= n)
+{
+    Console.Write($"{count} ");
+    count++;
+}
+
