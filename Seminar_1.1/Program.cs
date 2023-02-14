76924dae2d5a87cd1059b04ddc29554c650f5db2
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-14 18:13:52 +0300

    Принимает на вход число и выдает его квадрат ( число умножить на само себя)

---
+++ Seminar_1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Принимает на вход число и выдает его квадрат ( число умножить на само себя)
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+// Решение математически 
+int number = num * num;
+Console.WriteLine($"Квадрат {num} равен: {number}");
+
+// Решение с помощью библиотеки
+int num2 = Convert.ToInt32(Math.Pow(num, 2)); //Конвертируем с типа double в int. Math.Pow - возведение в степень
+Console.WriteLine($"Квадрат {num} равен: {num2}");
