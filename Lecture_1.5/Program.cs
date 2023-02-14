50b23324e92ff175de2779893d6ebd86b5801f42
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-14 17:03:52 +0300

    Оператор if else и  .ToLower()

---
+++ Lecture_1.5/Program.cs
@@ -0,0 +1,17 @@
+﻿// Оператор if else
+// .ToLower() - Позволяет выводить "Ура это же Маша", если мы водим слово Маша по познакому - мАША, МАШа, МАша
+
+
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша") 
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
