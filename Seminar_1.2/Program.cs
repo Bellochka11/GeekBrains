002f8ee499d55350b72ffa75302b5967f98a8248
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-14 19:14:48 +0300

    Семинар 1 Название дня недели по заданному номеру

---
+++ Seminar_1.2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Название дня недели по заданному номеру
+Console.WriteLine("Введите число: ");
+int  x = int.Parse(Console.ReadLine()!);
+if (x == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (x == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (x == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (x == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (x == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (x == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (x == 7)
+{
+    Console.WriteLine("Воскресенье");
+} 
+else if (x < 1 || x > 7)
+{
+    Console.WriteLine("Такого дня не существует");
+}
