606a4107b9c839388e45739d27d6e47c8b7dadea
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-14 19:49:00 +0300

    Вход трехзначное число. Выход последняя цифра этого числа.

---
+++ Seminar_1.4/Program.cs
@@ -0,0 +1,6 @@
+﻿// Вход трехзначное число. Выход последняя цифра этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int  n = int.Parse(Console.ReadLine()!);
+int a = n % 10;
+Console.WriteLine($"Последняя цифра этого числа: {a}");
+
