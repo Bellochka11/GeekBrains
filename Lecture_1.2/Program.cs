b1fe92862d847b4ce5d64217829142ff58ee96b7
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-14 16:25:16 +0300

    Сложение двух чисел. Лекция 1

---
+++ Lecture_1.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сложение 2 чисел
+
+int numberA = 3;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+// Другой вариант решения
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
